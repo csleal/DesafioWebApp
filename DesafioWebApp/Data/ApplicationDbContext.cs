e1965531369a18dad499a51e5a9216e73f880049
carlos leal <carlosr.souzaleal@gmail.com>
2023-09-18 18:01:51 -0300

    DBContext

--- DesafioWebApp/Data/ApplicationDbContext.cs
+++ DesafioWebApp/Data/ApplicationDbContext.cs
@@ -1,6 +1,16 @@
+using DesafioWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace DesafioWebApp.Data;
 
-public class ApplicationDbContext
+public class ApplicationDbContext : DbContext
 {
-    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Corrida> Corridas { get; set; }
+    public DbSet<Club> Clubs { get; set; }
+    public DbSet<Endereco> Enderecos { get; set; }
 }
