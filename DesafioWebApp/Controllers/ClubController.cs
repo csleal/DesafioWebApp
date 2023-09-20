c1eb3cc55d8a84a0eac0216d3cc2f642423241f5
carlos leal <carlosr.souzaleal@gmail.com>
2023-09-20 12:11:25 -0300

    http-post

--- DesafioWebApp/Controllers/ClubController.cs
+++ DesafioWebApp/Controllers/ClubController.cs
@@ -32,4 +32,16 @@ public class ClubController : Controller
     {
         return View();
     }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Club club)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(club);
+        }
+
+        _clubRepository.Add(club);
+        return RedirectToAction("Index");
+    }
 }
