233b4c184841c20f14899827c8c2674a9f38321a
carlos leal <carlosr.souzaleal@gmail.com>
2023-09-22 16:26:05 -0300

    adicição tema do bootstrap + melhoramento navbar e homepage

--- DesafioWebApp/ViewModels/HomeViewModel.cs
+++ DesafioWebApp/ViewModels/HomeViewModel.cs
@@ -7,5 +7,4 @@ public class HomeViewModel
     public IEnumerable<Club>? Clubs { get; set; }
     public string? City { get; set; }
     public string? State { get; set; }
-    // public HomeUserCreateViewModel Register { get; set; } = new HomeUserCreateViewModel();
 }
